0f949fab5121ff59e9f8196d05c77f8ca40124b3
Fychia <rafael.valerio@outlook.com.br>
2023-03-17 00:30:46 -0300

    new commit
    Title/Index/CSS

---
+++ todo_app/src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Title component
+export default function Title(){
+    return(
+        <div className="title">
+            <h1>To do List</h1>
+        </div>
+    );
+}
